8ccb01cbda4a0d5a8bd8962faccac4d8eea44082
Hyunjip <hyunjip0206@ajou.ac.kr>
2025-05-07 02:10:44 +0900

    Ray-Cast 구현에 앞서 리팩터했습니다. 나중에 모델/카메라/컨트롤러 정보를 연동할 수 있도록 style.css와 index.html도 미리 수정해두었습니다.

---
+++ robot/robot.js
@@ -0,0 +1,1 @@
+// Hierarchical Model Tree Structure
---
+++ scene/renderer.js
@@ -0,0 +1,65 @@
+let meshProgramInfo;
+let viewMatrix;
+let projectionMatrix;
+
+export function initRenderer(gl, program, eye, at) {
+    meshProgramInfo = {
+        program,
+        attribLocations: {
+            a_position: gl.getAttribLocation(program, "vPosition"),
+            a_normal: gl.getAttribLocation(program, "vNormal"),
+            a_texcoord: gl.getAttribLocation(program, "vTexCoord"),
+            a_color: gl.getAttribLocation(program, "vColor"),
+        },
+        uniformLocations: {
+            u_world: gl.getUniformLocation(program, "uWorld"),
+            u_view: gl.getUniformLocation(program, "uView"),
+            u_projection: gl.getUniformLocation(program, "uProjection"),
+            u_lightDirection: gl.getUniformLocation(program, "uLightDirection"),
+            u_diffuse: gl.getUniformLocation(program, "uDiffuse"),
+        },
+    };
+
+    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    const cameraMatrix = m4.lookAt(eye, at, [0, 1, 0]);
+
+    viewMatrix = m4.inverse(cameraMatrix);
+    projectionMatrix = m4.perspective(Math.PI / 4, aspect, 0.1, 1000);
+
+    return {viewMatrix, projectionMatrix};
+}
+
+export function renderScene(gl, meshes, time, objOffset = [0, 0, 0]) {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    gl.uniformMatrix4fv(meshProgramInfo.uniformLocations.u_view, false, viewMatrix);
+    gl.uniformMatrix4fv(meshProgramInfo.uniformLocations.u_projection, false, projectionMatrix);
+    gl.uniform3fv(meshProgramInfo.uniformLocations.u_lightDirection, m4.normalize([-1, 3, 5]));
+    gl.uniform4fv(meshProgramInfo.uniformLocations.u_diffuse, [1, 1, 1, 1]);
+
+    gl.enable(gl.DEPTH_TEST);
+
+    for (const mesh of meshes) {
+        gl.useProgram(meshProgramInfo.program);
+        let worldMatrix = mesh.transform || m4.identity();
+        if (mesh.name === 'spider') {
+            worldMatrix = m4.multiply(worldMatrix, m4.scaling(0.1, 0.1, 0.1));
+        }
+        gl.uniformMatrix4fv(meshProgramInfo.uniformLocations.u_world, false, worldMatrix);
+
+        bindAttrib(gl, mesh.buffers.position, meshProgramInfo.attribLocations.a_position, 3);
+        bindAttrib(gl, mesh.buffers.normal, meshProgramInfo.attribLocations.a_normal, 3);
+        bindAttrib(gl, mesh.buffers.texcoord, meshProgramInfo.attribLocations.a_texcoord, 2);
+        bindAttrib(gl, mesh.buffers.color, meshProgramInfo.attribLocations.a_color, 3);
+
+        gl.drawArrays(gl.TRIANGLES, 0, mesh.numElements);
+    }
+}
+
+function bindAttrib(gl, buffer, attrib, size) {
+    if (buffer && attrib >= 0) {
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+        gl.vertexAttribPointer(attrib, size, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(attrib);
+    }
+}
